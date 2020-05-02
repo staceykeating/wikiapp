25ca120f2e9d2a560c8a582c0c620dfc139960f2
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-02 22:37:46 +0000

    fixed seeds syntax

--- db/seeds/04_markers.sql
+++ db/seeds/04_markers.sql
@@ -1,4 +1,4 @@
-INSERT INTO markers (map_id, creator_id, title, description, image_url, latitutde, longitude)
+INSERT INTO markers (map_id, creator_id, title, description, image_url, latitude, longitude)
 VALUES (1, 1, '33 Acres', 'Best beer in town', 'https://images.pexels.com/photos/1089930/pexels-photo-1089930.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 49.264112, -123.105350),
-(2, 3, `Tacofino`, 'Best tacos in town!', 'https://images.pexels.com/photos/2092507/pexels-photo-2092507.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 49.2728373, -123.119373),
+(2, 3, 'Tacofino', 'Best tacos in town!', 'https://images.pexels.com/photos/2092507/pexels-photo-2092507.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 49.2728373, -123.119373),
 (3, 2, 'Mcdonalds', 'Best burgers in town!', 'https://images.pexels.com/photos/1858115/pexels-photo-1858115.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 49.279762, -123.115518);
