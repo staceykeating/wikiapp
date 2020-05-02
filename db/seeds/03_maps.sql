0b9f3895b53b335265e9061e6e6be335d38c62a4
Stacey Keating <staceyamberkk@gmail.com>
2020-05-02 22:44:52 +0000

    error remove

--- db/seeds/03_maps.sql
+++ db/seeds/03_maps.sql
@@ -1,10 +1,6 @@
 -- Maps table seeds
 
-<<<<<<< HEAD
-INSERT INTO maps (title, description, zoom, creator_id) VALUES ('Best Breweries', 'Checkout the best places to get some cold brews in Vancity!', 10, 1);
-=======
 INSERT INTO maps (title, description, zoom, creator_id) VALUES ('Best Breweries', 'Checkout the best places to get some cold brews in Vancity!', 10, 1)
->>>>>>> 60f459c0c4e26bb46ccfac1a7475e6609940c940
 INSERT INTO maps (title, description, zoom, creator_id) VALUES ('Tasty Tacos', 'My got to places to curb my taco cravings!', 10, 2);
 INSERT INTO maps (title, description, zoom, creator_id) VALUES ('Fast Food Feasting', 'My cure for all munchies!', 10, 3);
 
--- db/seeds/04_markers.sql
+++ db/seeds/04_markers.sql
@@ -1,9 +1,4 @@
 INSERT INTO markers (map_id, creator_id, title, description, image_url, latitude, longitude)
 VALUES (1, 1, '33 Acres', 'Best beer in town', 'https://images.pexels.com/photos/1089930/pexels-photo-1089930.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 49.264112, -123.105350),
-<<<<<<< HEAD
 (2, 3, 'Tacofino', 'Best tacos in town!', 'https://images.pexels.com/photos/2092507/pexels-photo-2092507.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 49.2728373, -123.119373),
 (3, 2, 'Mcdonalds', 'Best burgers in town!', 'https://images.pexels.com/photos/1858115/pexels-photo-1858115.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 49.279762, -123.115518);
-=======
-(2, 3, `Tacofino`, 'Best tacos in town!', 'https://images.pexels.com/photos/2092507/pexels-photo-2092507.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 49.2728373, -123.119373),
-(3, 2, 'Mcdonalds', 'Best burgers in town!', 'https://images.pexels.com/photos/1858115/pexels-photo-1858115.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 49.279762, -123.115518);
->>>>>>> cfd37134ca2ad32c2ae91d43f53ec195a3ddd4eb
