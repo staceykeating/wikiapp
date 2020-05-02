a869c87a9b749b7043a8befee22dc4e8f0672fd8
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-02 22:17:32 +0000

    Added marker seeds

---
+++ db/seeds/04_markers.sql
@@ -0,0 +1,4 @@
+INSERT INTO markers (map_id, creator_id, title, description, image_url, latitutde, longitude)
+VALUES (1, 1, '33 Acres', 'Best beer in town', img.png, 49.264112, -123.105350),
+(2, 3, `Tacofino`, 'Best tacos in town!', img.png, 49.2728373, -123.119373),
+(3, 2, 'Mcdonalds', 'Best burgers in town!', img.png, 49.279762, -123.115518);
