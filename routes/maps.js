dc502ee933ebf29b39f8abefd80a3d86e38e7ab6
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-03 20:05:40 +0000

    Created routes and connected DB

--- routes/maps.js
+++ routes/maps.js
@@ -2,25 +2,51 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.put("/:map_id", (req, res) => {
-    let query = `SELECT * FROM maps WHERE id = $1`;
-    console.log(req.get('map_id'));
+  router.get("/", (req, res) => {
+    db.query('SELECT * FROM favorites;')
+    .then(res => {
+      console.log(res.rows[0])
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 
-    let map = db.query(query, [req.get('map_id')])
-      .then(data => {
-        const maps = data.rows;
-        res.json({ maps });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-      // i want to search map api, get result and save that result into my markers database
+    res.render("index");
   });
 
-  router.post('/search', (req, res) => {
-    let searchValues = req.get('q');
+  router.get('/maps', (req, res) => {
+    res.render("edits");
   });
+
+  router.get('/profile', (req, res) => {
+    res.render("profile")
+  })
+
+  router.get('/create', (req, res) => {
+    res.render("create")
+  })
+  
+  
+
+  // router.put("/:map_id", (req, res) => {
+  //   let query = `SELECT * FROM maps WHERE id = $1`;
+  //   console.log(req.get('map_id'));
+
+  //   let map = db.query(query, [req.get('map_id')])
+  //     .then(data => {
+  //       const maps = data.rows;
+  //       res.json({ maps });
+  //     })
+  //     .catch(err => {
+  //       res
+  //         .status(500)
+  //         .json({ error: err.message });
+  //     });
+  //     // i want to search map api, get result and save that result into my markers database
+  // });
+
+  // router.post('/search', (req, res) => {
+  //   let searchValues = req.get('q');
+  // });
   return router;
 };
--- routes/profile.js
+++ routes/profile.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router  = express.Router();
 
 
-
-
-
-
   module.exports = (db) => {
+
+    
     router.get("/profile", (req, res) => {
       res.render("urls_profile");
     });
