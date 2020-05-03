b4295871f9bdc4aae5e5dc11ff30e90b1048d005
vagrant <j_khabra11@hotmail.com>
2020-05-03 17:37:59 +0000

    create ejs files for homepage/ nav/ edits

--- server.js
+++ server.js
@@ -35,11 +35,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const mapsRoutes = require("./routes/maps");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/maps", mapsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
@@ -48,6 +50,10 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get('/maps/edit', (req, res) => {
+  res.render("edits");
 });
 
 app.listen(PORT, () => {
