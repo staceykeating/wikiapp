b123527c92b7c528ce6499a04220178ac3511eea
Stacey Keating <staceyamberkk@gmail.com>
2020-05-03 05:50:04 +0000

    create pages profile and create

---
+++ routes/profile.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router  = express.Router();
+
+
+
+
+
+
+  module.exports = (db) => {
+    router.get("/profile", (req, res) => {
+      res.render("urls_profile");
+    });
+    return router;
+  };
+
