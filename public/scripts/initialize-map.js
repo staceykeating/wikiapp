fb47e56c81ca74eb4e2dc619ba89575117b9adc5
Stacey Keating <staceyamberkk@gmail.com>
2020-05-05 20:15:37 +0000

    button update

--- public/scripts/initialize-map.js
+++ public/scripts/initialize-map.js
@@ -2,8 +2,14 @@ let _map;
 const initialize = function(map) {
     // creates a class for this particular map
     const newDiv = `map_${map.id}`;
+    const newMapTemplate= `<h2>${map.map_title}</h2> <div class="maps ${newDiv}"></div>
+    <div class="icon">
+      <form method="POST" action="/create"><button type="submit" i class="fas fa-heart"></i></button></form>
+      <form method="POST" action="/edit"><button type="submit" i class="fas fa-edit"></i></button></form></div>`;
+
+
     // appends a new div with the newDiv class for this particular map to live
-    $(`#maps_container`).append(`<h1>${map.map_title}<h1> <div class="maps ${newDiv}"></div>`);
+    $(`#maps_container`).append(newMapTemplate);
     // creates an instance of a map
     _map = new google.maps.Map($(`.${newDiv}`)[0], {
       center: {lat: 49.2827, lng: -123.1207}, // all maps are of vancouver
@@ -15,7 +21,7 @@ const initialize = function(map) {
     const markers = map.markers;
 
     for (const marker of markers) {
-      
+
       // Initialize info window and insert content as html
       let infowindow = new google.maps.InfoWindow({
         content: `<h4>${marker.marker_title}</h4>`
